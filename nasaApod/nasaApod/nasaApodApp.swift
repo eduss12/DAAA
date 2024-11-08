4ee2fc4e2bc63710af27e0feb142ce4cfd78a05d
eduss12 <147599886+eduss12@users.noreply.github.com>
2024-11-08 12:23:58 +0100

    Add files via upload

---
+++ nasaApod/nasaApod/nasaApodApp.swift
@@ -0,0 +1,17 @@
+//
+//  nasaApodApp.swift
+//  nasaApod
+//
+//  Created by alumno on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct nasaApodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
